6b3c6b5e539bd30708175563f384e8d26e852eb4
zhabgraham <115187364+zhabgraham@users.noreply.github.com>
2022-10-21 23:24:45 +0300

    Add files via upload

--- Lab_03_04/Lab_03_04.cpp
+++ Lab_03_04/Lab_03_04.cpp
@@ -13,27 +13,18 @@ int main()
     double x; // вхідний аргумент
     double y; // вхідний параметр
     double R; // вхідний параметр
-    double xc1, xc2, yc1, yc2; // координати центрів кіл
-    double d1, d2, d3, d4;
 
     cout << "x = "; cin >> x; 
     cout << "y = "; cin >> y;
     cout << "R = "; cin >> R;
 
-    yc1 = R;
-    xc1 = R * (-1);
-    yc2 = R * (-1);
-    xc2 = R;
-    d1 = x - xc1;
-    d2 = y - yc1;
-    d3 = x - xc2;
-    d4 = y - yc2;
-
     // розгалуження в повній формі
-    if ((-1) * R < x && x < R && ((-1) * R < y && y < R) && (sqrt(pow(d1, 2) + pow(d2, 2)) > R) && (sqrt(pow(d3, 2) + pow(d4, 2)) > R))
-        cout << "yes";
+    if ( ((x + R)*(x + R) + (y - R)*(y - R) >= R*R && y >= 0 && x <= 0 && y <= R && x <= R)
+        || ((x - R) * (x - R) + (y + R) * (y + R) >= R*R && y <= 0 && x >= 0 && y <= R && x <= R)
+        || (x <= R && y <= R && x >= 0 && y >= 0) || (x <= 0 && y <= 0 && x >= -R && y >= -R))
+        cout << "yes" << endl;
     else 
-        cout << "no";
+        cout << "no" << endl;
 
     system("pause");
     return 0;
